16f099c50a68ff722a92f1f9ed25d7f2b3a43e63
CHITHRA2413 <chithrapandi9686@gmail.com>
2023-07-30 16:13:54 +0530

    js and html

---
+++ MODULE2/OBJECT CREATE/objectcreate.js
@@ -0,0 +1,13 @@
+//CREATING AN OBJECT USING OBJECT LITERAL NOTATION
+const person = {
+    firstName : "John",
+    lastName : "Doe",
+    age : 30,
+    occupation : "Engineer",
+};
+//Accessing object properties using dot notation
+console.log(person.firstName); //output:John
+console.log(person.lastName);  //output:Doe
+console.log(person.age);       //output:30
+console.log(person.occupation);  //output: Engineer
+//Accessing object properties using bracket notation
